082932c2a5edfad1dcabdd58857daf6788630d08
David Strauch <dmaster18@gmail.com>
2021-01-24 04:36:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function (collection, callback) {
+      if(Array.isArray(collection)) {
+        for (let i = 0; i < collection.length; i++) {
+          callback(collection[i], i, collection)
+        }
+      } else {
+        for (let i = 0; i < Object.keys(collection).length; i++) {
+          callback(collection[Object.keys(collection)[i]], Object.keys(collection)[i], i)
+        }
+      }
+      return collection;
     },
 
     map: function() {
